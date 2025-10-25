78bfa47d391dd8f46144e3dfd7304c299898eb19
Rifushigi <rifushigi.teda@gmail.com>
2025-10-25 20:37:02 +0100

    feat: extend CountryRepository with query and specification methods

--- src/main/java/com/rifushigi/nomisma/repository/CountryRepository.java
+++ src/main/java/com/rifushigi/nomisma/repository/CountryRepository.java
@@ -1,12 +1,26 @@
 package com.rifushigi.nomisma.repository;
 
 import com.rifushigi.nomisma.entity.Country;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, UUID> {
 
+    List<Country> findAll(Specification<Country> spec, Sort sort);
+
+    Optional<Country> getCountryByName(String name);
+
+    long deleteCountryByName(String name);
+
+    @Query("from Country c select max(c.lastRefreshedAt)")
+    Instant findLastRefreshedAt();
 }
