b951ac82aa50c102a2492af912f263541b56a4ff
Rifushigi <rifushigi.teda@gmail.com>
2025-10-25 13:58:15 +0100

    feat: add HttpStatus field to BaseException and update constructor

--- src/main/java/com/rifushigi/nomisma/exception/BaseException.java
+++ src/main/java/com/rifushigi/nomisma/exception/BaseException.java
@@ -1,6 +1,7 @@
 package com.rifushigi.nomisma.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.time.Instant;
 
@@ -9,10 +10,12 @@ public class BaseException extends RuntimeException{
 
     private final String details;
     private final Instant timestamp;
+    private final HttpStatus status;
 
-    protected BaseException(String message, String details){
+    protected BaseException(String message, String details, HttpStatus status){
         super(message);
         this.details = details;
+        this.status = status;
         this.timestamp = Instant.now();
     }
 }
