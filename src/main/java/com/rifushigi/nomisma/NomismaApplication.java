6de2ed89100a28aabfc1c1cc5e8391ef71d318a4
Rifushigi <rifushigi.teda@gmail.com>
2025-10-25 10:47:42 +0100

    chore: project scaffold

---
+++ src/main/java/com/rifushigi/nomisma/NomismaApplication.java
@@ -0,0 +1,13 @@
+package com.rifushigi.nomisma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomismaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomismaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rifushigi/nomisma/NomismaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifushigi.nomisma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomismaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
