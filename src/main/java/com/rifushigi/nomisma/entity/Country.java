ec2aefb134b5ed35551041aee9c606b7a17e9a63
Rifushigi <rifushigi.teda@gmail.com>
2025-10-25 12:07:11 +0100

    feat: add Country entity

---
+++ src/main/java/com/rifushigi/nomisma/entity/Country.java
@@ -0,0 +1,45 @@
+package com.rifushigi.nomisma.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String capital;
+
+    private String region;
+
+    @Column(nullable = false)
+    private Long population;
+
+    @Column(name = "currency_code", nullable = false)
+    private String currencyCode;
+
+    @Column(name = "exchange_rate", nullable = false)
+    private Double exchangeRate;
+
+    @Column(name = "estimated_gdp", nullable = false)
+    private Long estimatedGdp;
+
+    @Column(name = "flag_url")
+    private String flagUrl;
+
+    @LastModifiedDate
+    private LocalDateTime lastRefreshedAt;
+}
