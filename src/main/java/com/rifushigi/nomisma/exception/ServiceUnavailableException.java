558db06d7f31ef4ff83037d528146e2fa731ba53
Rifushigi <rifushigi.teda@gmail.com>
2025-10-25 13:59:16 +0100

    feat: update ServiceUnavailableException to include HttpStatus.SERVICE_UNAVAILABLE

--- src/main/java/com/rifushigi/nomisma/exception/ServiceUnavailableException.java
+++ src/main/java/com/rifushigi/nomisma/exception/ServiceUnavailableException.java
@@ -1,8 +1,10 @@
 package com.rifushigi.nomisma.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class ServiceUnavailableException extends BaseException{
 
     protected ServiceUnavailableException(String message, String details) {
-        super(message, details);
+        super(message, details, HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
