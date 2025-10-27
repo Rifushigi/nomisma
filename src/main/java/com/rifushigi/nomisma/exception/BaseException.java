f1b8ba76aa09a0b2700d888d00a719dffff8420f
Rifushigi <rifushigi.teda@gmail.com>
2025-10-27 11:29:23 +0100

    feat: extend BaseException with overloaded constructor to support detailed error information

--- src/main/java/com/rifushigi/nomisma/exception/BaseException.java
+++ src/main/java/com/rifushigi/nomisma/exception/BaseException.java
@@ -4,15 +4,23 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.time.Instant;
+import java.util.Map;
 
 @Getter
 public class BaseException extends RuntimeException{
 
-    private final String details;
+    private final Object details;
     private final Instant timestamp;
     private final HttpStatus status;
 
     protected BaseException(String message, String details, HttpStatus status){
+        super(message);
+        this.details = details;
+        this.status = status;
+        this.timestamp = Instant.now();
+    }
+
+    protected BaseException(String message, Map<String, String> details, HttpStatus status){
         super(message);
         this.details = details;
         this.status = status;
