fcabb53de6204d3ac2b3f22d04c5fd7c7d13a960
Rifushigi <rifushigi.teda@gmail.com>
2025-10-27 23:04:59 +0100

    feat: update global exception handling

--- src/main/java/com/rifushigi/nomisma/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rifushigi/nomisma/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.rifushigi.nomisma.exception;
 
 import com.rifushigi.nomisma.dto.ErrorResponseDTO;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,6 +26,35 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(ex.getStatus()).body(response);
     }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleConstraintViolation(ConstraintViolationException ex) {
+
+        Map<String, String> details = new HashMap<>();
+        ex.getConstraintViolations().forEach(v -> {
+            String field = v.getPropertyPath().toString();
+            details.put(field, v.getMessage());
+        });
+
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                "Validation failed",
+                details,
+                Instant.now().toString()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponseDTO> handleIllegalArg(IllegalArgumentException ex) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                "Validation failed",
+                Map.of("name", ex.getMessage()),
+                Instant.now().toString()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDTO> handleValidation(
