92193989aa4ec6947fbff5cba018759f024dd2a9
Rifushigi <rifushigi.teda@gmail.com>
2025-10-27 16:41:47 +0100

    refactor: update CountryService to use specific DTOs and projections
    - Replaced `CountryFilter` with `CountryFilterDTO` in `getAllCountries` method.
    - Changed return type of `getCountriesWithRefreshTimestamp` to `CountrySummaryResponseDTO`.
    - Updated `generateSummaryImage` parameters to use `CountryGdpProjection` and added a `timestamp` argument.

--- src/main/java/com/rifushigi/nomisma/service/CountryService.java
+++ src/main/java/com/rifushigi/nomisma/service/CountryService.java
@@ -1,21 +1,19 @@
 package com.rifushigi.nomisma.service;
 
-import com.rifushigi.nomisma.dto.CountryFilter;
-import com.rifushigi.nomisma.dto.CountrySummaryResponse;
+import com.rifushigi.nomisma.dto.CountryFilterDTO;
+import com.rifushigi.nomisma.dto.CountrySummaryResponseDTO;
 import com.rifushigi.nomisma.entity.Country;
-import org.springframework.data.jpa.domain.Specification;
+import com.rifushigi.nomisma.projection.CountryGdpProjection;
 
 import java.io.File;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public interface CountryService {
-    List<Country> getAllCountries(CountryFilter filters);
+    List<Country> getAllCountries(CountryFilterDTO filters);
     Country getCountryByName(String name);
     void deleteCountryByName(String name);
-    CountrySummaryResponse getCountriesWithRefreshTimestamp();
+    CountrySummaryResponseDTO getCountriesWithRefreshTimestamp();
     void refreshCountries();
-    void generateSummaryImage(long totalCountries, List<String> top5ByGdp);
+    void generateSummaryImage(long totalCountries, List<CountryGdpProjection> top5ByGdp, String timestamp);
     File getSummaryImage();
 }
