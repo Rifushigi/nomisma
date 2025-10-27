059e33885b5dff6ed1000855e9fbd2875db3e97e
Rifushigi <rifushigi.teda@gmail.com>
2025-10-27 21:59:23 +0100

    feat: add CountryController for handling country-related endpoints
    - Introduced `getCountries` to fetch all countries with filtering options.
    - Implemented `getCountryByName` to retrieve a country by its name.
    - Added `deleteCountryByName` to delete a country by name with validation.
    - Created `getStatus` to provide a summary of countries and last refresh timestamp.
    - Added `getSummaryImage` to serve the cached summary image of top GDP countries.
    - Implemented `refresh` to refresh country data and regenerate summaries.

---
+++ src/main/java/com/rifushigi/nomisma/controller/CountryController.java
@@ -0,0 +1,77 @@
+package com.rifushigi.nomisma.controller;
+
+import com.rifushigi.nomisma.dto.CountryFilterDTO;
+import com.rifushigi.nomisma.dto.CountrySummaryResponseDTO;
+import com.rifushigi.nomisma.entity.Country;
+import com.rifushigi.nomisma.service.impl.CountryServiceImpl;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class CountryController {
+
+    private final CountryServiceImpl countryService;
+
+    @GetMapping
+    public ResponseEntity<List<Country>> getCountries(CountryFilterDTO filters){
+        List<Country> response = countryService.getAllCountries(filters);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping(path = "countries/{name}")
+    public ResponseEntity<Country> getCountryByName(@PathVariable("name") String name){
+        Country response = countryService.getCountryByName(name);
+        return ResponseEntity.ok(response);
+    }
+
+    @Validated
+    @DeleteMapping(path = "countries/{name}")
+    public ResponseEntity<Void> deleteCountryByName(
+            @PathVariable("name")
+            @NotBlank(message = "Country name must not be blank")String name){
+        countryService.deleteCountryByName(name);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("status")
+    public ResponseEntity<CountrySummaryResponseDTO> getStatus(){
+        CountrySummaryResponseDTO response = countryService.getCountriesWithRefreshTimestamp();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("countries/image")
+    public ResponseEntity<InputStreamSource> getSummaryImage() throws FileNotFoundException {
+        File imageFile = countryService.getSummaryImage();
+        InputStreamSource resource = new InputStreamResource(new FileInputStream(imageFile));
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"summary.png\"")
+                .contentLength(imageFile.length())
+                .contentType(MediaType.IMAGE_PNG)
+                .body(resource);
+    }
+
+    @PostMapping("countries/refresh")
+    public ResponseEntity<Void> refresh(){
+        countryService.refreshCountries();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
