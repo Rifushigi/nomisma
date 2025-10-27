1592ee46781b2bbe9b4fa4dd7135093c33782f8a
Rifushigi <rifushigi.teda@gmail.com>
2025-10-27 23:28:54 +0100

    feat: create a database seeder

---
+++ src/main/java/com/rifushigi/nomisma/bootstrap/DatabaseSeeder.java
@@ -0,0 +1,28 @@
+package com.rifushigi.nomisma.bootstrap;
+
+import com.rifushigi.nomisma.repository.CountryRepository;
+import com.rifushigi.nomisma.service.impl.CountryServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DatabaseSeeder implements CommandLineRunner {
+
+    private final CountryServiceImpl countryService;
+    private final CountryRepository countryRepository;
+
+    @Override
+    public void run(String... args) {
+        if (countryRepository.count() == 0) {
+            log.info("Seeding countries...");
+            countryService.fetchAllCountries();
+            System.out.println("Database seeding completed!");
+        } else {
+            System.out.println("Database already contains data. Skipping seeding.");
+        }
+    }
+}
