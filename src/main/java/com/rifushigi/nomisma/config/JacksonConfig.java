e48ef92af04772fa346a746068aa70a8bded836d
Rifushigi <rifushigi.teda@gmail.com>
2025-10-28 11:54:49 +0100

    feat: update jackson config so that it customizes the default instead of replacing it

--- src/main/java/com/rifushigi/nomisma/config/JacksonConfig.java
+++ src/main/java/com/rifushigi/nomisma/config/JacksonConfig.java
@@ -1,9 +1,9 @@
 package com.rifushigi.nomisma.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class JacksonConfig {
 
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-        return mapper;
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> builder
+                .modules(new JavaTimeModule())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
     }
 }
+
 
 
