959877ab1641978ddf470ed21afa56906f10d447
Rifushigi <rifushigi.teda@gmail.com>
2025-10-28 10:31:31 +0100

    feat: enhance Jackson configuration to support Java 8 time types and disable timestamp serialization

--- src/main/java/com/rifushigi/nomisma/config/JacksonConfig.java
+++ src/main/java/com/rifushigi/nomisma/config/JacksonConfig.java
@@ -2,6 +2,8 @@ package com.rifushigi.nomisma.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,8 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         return mapper;
     }
