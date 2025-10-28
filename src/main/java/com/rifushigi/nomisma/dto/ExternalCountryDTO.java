94471f81830818852c8ab670e5f45109f6ffc3e3
Rifushigi <rifushigi.teda@gmail.com>
2025-10-28 11:50:52 +0100

    feat: add annotation to tell jackson to ignore fields not specified in the dto

--- src/main/java/com/rifushigi/nomisma/dto/ExternalCountryDTO.java
+++ src/main/java/com/rifushigi/nomisma/dto/ExternalCountryDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ExternalCountryDTO(
         String name,
         String capital,
