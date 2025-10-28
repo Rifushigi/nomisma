63f08a55ed9b4a2cfce255fc9376d03b30dafd1f
Rifushigi <rifushigi.teda@gmail.com>
2025-10-28 09:37:05 +0100

    feat: enhance RestClient configuration with custom HttpClient and connection timeout

--- src/main/java/com/rifushigi/nomisma/config/RestClientConfig.java
+++ src/main/java/com/rifushigi/nomisma/config/RestClientConfig.java
@@ -3,21 +3,32 @@ package com.rifushigi.nomisma.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
     public RestClient countryApi (RestClient.Builder builder){
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
         return builder
                 .baseUrl("https://restcountries.com/v2")
+                .requestFactory(new JdkClientHttpRequestFactory(httpClient))
                 .build();
     }
 
     @Bean
     public RestClient exchangeRateApi (RestClient.Builder builder){
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
         return builder
                 .baseUrl("https://open.er-api.com/v6/latest")
+                .requestFactory(new JdkClientHttpRequestFactory(httpClient))
                 .build();
     }
 }
