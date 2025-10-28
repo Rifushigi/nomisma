1552c069fb7f66df5e4a350f8730f0edc835ca82
Rifushigi <rifushigi.teda@gmail.com>
2025-10-28 01:50:21 +0100

    feat: replace v2 with v3

--- src/main/resources/db/migration/V2__.sql
+++ src/main/resources/db/migration/V2__.sql
@@ -1,14 +1,2 @@
-CREATE TABLE country
-(
-    id                VARCHAR(255) NOT NULL,
-    name              VARCHAR(255) NOT NULL,
-    capital           VARCHAR(255) NULL,
-    region            VARCHAR(255) NULL,
-    population        BIGINT       NOT NULL,
-    currency_code     VARCHAR(255) NULL,
-    exchange_rate     DECIMAL NULL,
-    estimated_gdp     DECIMAL(20, 2) NULL,
-    flag_url          VARCHAR(255) NULL,
-    last_refreshed_at datetime NULL,
-    CONSTRAINT pk_country PRIMARY KEY (id)
-);
+ALTER TABLE country
+    MODIFY estimated_gdp DECIMAL (20, 2);
--- src/main/resources/db/migration/V3__.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE country
-    MODIFY estimated_gdp DECIMAL (20, 2);
