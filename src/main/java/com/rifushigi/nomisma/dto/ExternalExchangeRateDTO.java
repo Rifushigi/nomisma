a3a104c4d1b7fe53763680f773a34273dbecc005
Rifushigi <rifushigi.teda@gmail.com>
2025-10-28 11:55:02 +0100

    feat: add jackson annotation

--- src/main/java/com/rifushigi/nomisma/dto/ExternalExchangeRateDTO.java
+++ src/main/java/com/rifushigi/nomisma/dto/ExternalExchangeRateDTO.java
@@ -1,9 +1,11 @@
 package com.rifushigi.nomisma.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ExternalExchangeRateDTO(
     String result,
     @JsonProperty("base_code") String baseCode,
